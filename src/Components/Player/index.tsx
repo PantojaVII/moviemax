ab595065aa5253dff6403f7d9f06c95740822e39
Pantoja <jvpantojamaciel@gmail.com>
2024-03-02 13:34:53 -0300

    FRONT END MOVIEMAX VERSION 1.0.0

--- src/Components/Player/index.tsx
+++ src/Components/Player/index.tsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { FaPlay } from "react-icons/fa";
 import { ContainerPlayerStyled, PosterStyled, PlayerStyledShadow, StartStyled } from "./PlayerStyled";
-import Plyr, { APITypes } from "plyr-react";
-import "plyr-react/plyr.css";
 import {
   Player,
   BigPlayButton,
@@ -26,45 +24,30 @@ interface MoviePlayerProps {
 
 }
 
-
-
 export default function MoviePlayer({ poster, size, urlPlayer, duration }: MoviePlayerProps) {
   const [videoLoaded, setVideoLoaded] = useState<boolean>(false);
   const urlVideo = urlPlayer;
-  const ref = useRef<APITypes>(null);
+
 
   const handleStartClick = () => {
     setVideoLoaded(true);
   };
-  const videoOptions = {
-    controls: ['play', 'progress', 'duration', 'mute', 'volume', 'settings', 'fullscreen', "displayDuration",],
-    autoplay: false,
-    loop: { active: false },
-    volume: 0.7,
-    displayDuration: true,
-    loading: "<div class='plyr__loading'><span class='plyr__sr-only'>Carregando</span><div class='plyr__spinner'><div class='plyr__double-bounce1'></div><div class='plyr__double-bounce2'></div></div></div>"
-  };
-  const plyrVideo = (
-    urlPlayer && (
-      <Plyr
-        ref={ref}
-        options={videoOptions}
-        poster={poster}
-        source={{
-          type: "video",
-          sources: [
-            {
-              src: urlPlayer,
-            },
-          ],
-        }}
-      />
-    )
-  );
-  
+
   return (
     <ContainerPlayerStyled>
-      {plyrVideo}
+        <Player poster={poster} autoPlay >
+          <source src={urlVideo} />
+          <BigPlayButton position="center" />
+          <ControlBar>
+            <PlayToggle />
+            <ReplayControl seconds={10} />
+            <ForwardControl seconds={10} />
+            <VolumeMenuButton vertical />
+            <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} />
+            <CurrentTimeDisplay order={4.1} />
+          </ControlBar>
+        </Player>
     </ContainerPlayerStyled>
+
   );
 }
